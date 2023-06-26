dcd3640c6fb2483a86f88572662c788e3c544cd6
Student03 <student03@HackaTruck03.local>
2023-06-26 14:15:46 -0300

    inicio do quinto projeto - clone de telas similares ao spotify

---
+++ quintoProjeto/quintoProjeto/quintoProjetoApp.swift
@@ -0,0 +1,17 @@
+//
+//  quintoProjetoApp.swift
+//  quintoProjeto
+//
+//  Created by Student03 on 26/06/23.
+//
+
+import SwiftUI
+
+@main
+struct quintoProjetoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
