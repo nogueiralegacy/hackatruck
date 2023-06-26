96297923f563d946c16b58c3d6bd3e80bcf18be1
Student03 <student03@HackaTruck03.local>
2023-06-26 16:55:46 -0300

    primeira tela

--- quintoProjeto/quintoProjeto/ContentView.swift
+++ quintoProjeto/quintoProjeto/ContentView.swift
@@ -1,18 +1,96 @@
-//
-//  ContentView.swift
-//  quintoProjeto
-//
-//  Created by Student03 on 26/06/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    var musicas: [Musica] = [
+        Musica(nome: "Fogo", artista: ["Naldo Benny"], capa: URL(string: "https://www.jornaldorap.com.br/wp-content/uploads/2020/04/naldo_benny_live.jpg")!),
+        Musica(nome: "Love, Love", artista: ["Naldo Benny", "Melody"], capa: URL(string: "https://portalpopline.com.br/wp-content/uploads/2020/06/naldo-benny-yuri.jpg")!),
+        Musica(nome: "Fogo", artista: ["Naldo Benny"], capa: URL(string: "https://uploads.metropoles.com/wp-content/uploads/2023/02/22152127/Naldo-Benny-4-1024x686.jpg")!),
+        Musica(nome: "Fogo", artista: ["Naldo Benny"], capa: URL(string: "https://media.fstatic.com/DH5yngg4vQQdK9csLNLAzsPJ9N8=/full-fit-in/290x478/filters:format(webp)/media/artists/avatar/2022/12/naldo-benny_a305866.jpg")!),
+        Musica(nome: "Fogo", artista: ["Naldo Benny"], capa: URL(string: "https://odia.ig.com.br/_midias/jpg/2017/12/14/naldo1-857181.jpg")!),
+        Musica(nome: "Fogo", artista: ["Naldo Benny"], capa: URL(string: "https://i0.statig.com.br/bancodeimagens/7s/cy/d0/7scyd0st68s1zaemlorvcmoqh.jpg")!),
+        Musica(nome: "Fogo", artista: ["Naldo Benny"], capa: URL(string: "https://www.bandab.com.br/wp-content/uploads/2023/03/Naldo-Benny.jpg.webp")!),
+        Musica(nome: "Fogo", artista: ["Naldo Benny"], capa: URL(string: "https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/05/22/naldobenny_341907036_1278959106351215_3252508067350636649_n-1h81zwl8bf8te.jpg")!),
+    ]
+
+    
+    
+    
+    
     var body: some View {
         VStack {
-            Image("caminhao")
+            
+            HStack {
+                Image("caminhao")
+                    .resizable()
+            }
+            .frame(width: 200, height: 200)
+            .padding(.top, 50)
+            
+            VStack(alignment: .leading) {
+                Text("HacktruckFM")
+                    .font(.title)
+                    .bold()
+                    
+                HStack {
+                    Image("caminhao")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                    Text("Daniel Nogueira")
+                        .font(.subheadline)
+                }
+            }
+            .foregroundColor(.white)
+            .padding(.top, 20)
+            .offset(x: -90)
+            Spacer()
+            
+            ScrollView {
+                VStack(alignment: .leading) {
+                
+                    ForEach(musicas) { musica in
+                        HStack {
+                            AsyncImage(url: musica.capa) {image in
+                                image
+                                    .resizable()
+                                    .frame(width: 40, height: 40)
+                            }
+                        placeholder: {
+                            ProgressView()
+                        }
+                            VStack(alignment: .leading) {
+                                Text(musica.nome)
+                                    .font(.subheadline)
+                                
+                                HStack {
+                                    ForEach(musica.artista, id: \.self) {
+                                        artista in Text(artista)
+                                            .font(.caption)
+                                        
+                                    }
+                                }
+                            }
+                            .foregroundColor(.white)
+                            
+                            
+                            Image(systemName: "ellipsis")
+                                .colorInvert()
+                                .alignmentGuide(HorizontalAlignment.trailing) { d in
+                                    d.width}
+                        }
+                    }
+    
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .offset(x: -90)
+            }
+            .padding(.top, 30)
         }
-    }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(
+            LinearGradient(gradient: Gradient(colors: [.black, .blue]), startPoint: .bottom, endPoint: .top)
+        )
+          }
 }
 
 struct ContentView_Previews: PreviewProvider {
--- quintoProjeto/quintoProjeto/Musica.swift
+++ quintoProjeto/quintoProjeto/Musica.swift
@@ -1,15 +1,15 @@
 import Foundation
 
 class Musica: Identifiable {
-    let id: UUID
-    let nome: String
-    let artista: [String]
-    let capa: URL
-    
-    init(nome: String, artista: [String], capa: URL) {
-        self.id = UUID()
-        self.nome = nome
-        self.artista = artista
-        self.capa = capa
-    }
+        let id: UUID
+        let nome: String
+        let artista: [String]
+        let capa: URL
+        
+        init(nome: String, artista: [String], capa: URL) {
+            self.id = UUID()
+            self.nome = nome
+            self.artista = artista
+            self.capa = capa
+        }
 }
