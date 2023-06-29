c5223a06a3d6e03f0394a3c1b8adbf46d27adce4
Student03 <student03@HackaTruck03.local>
2023-06-29 15:33:03 -0300

    estrutura sexto projeto

---
+++ sextoProjeto/sextoProjeto/sextoProjetoApp.swift
@@ -0,0 +1,17 @@
+//
+//  sextoProjetoApp.swift
+//  sextoProjeto
+//
+//  Created by Student03 on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct sextoProjetoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
