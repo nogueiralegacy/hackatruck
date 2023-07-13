7f7c90f563dc6bb122154d9b342ada2a1d922434
Student03 <student03@HackaTruck03.local>
2023-07-13 14:58:21 -0300

    projeto final

---
+++ projetoFinal/aplicativo/aplicativo/aplicativoApp.swift
@@ -0,0 +1,17 @@
+//
+//  aplicativoApp.swift
+//  aplicativo
+//
+//  Created by Student03 on 13/07/23.
+//
+
+import SwiftUI
+
+@main
+struct aplicativoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
